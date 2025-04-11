b22824b73af3054f50a7885dacf2ee2594a836b0
Pierrick Turelier <pierrick@turelier.com>
2025-04-10 19:00:24 -0500

    feat: add YieldFee component

---
+++ src/interfaces/IMYieldFee.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity 0.8.26;
+
+/**
+ * @title Interface for Yield Fee M extension.
+ * @author M^0 Labs
+ */
+interface IMYieldFee {
+    /* ============ Events ============ */
+
+    /**
+     * @notice Emitted when an account's yield is claimed.
+     * @param  claimer   The address that claimed the yield fee.
+     * @param  recipient The address of the recipient.
+     * @param  yield     The amount of M yield claimed.
+     */
+    event YieldClaimed(address indexed claimer, address indexed recipient, uint256 yield);
+
+    /**
+     * @notice Emitted when the yield fee is claimed.
+     * @param  claimer   The address that claimed the yield fee.
+     * @param  recipient The address of the recipient.
+     * @param  yieldFee  The amount of yield fee claimed.
+     */
+    event YieldFeeClaimed(address indexed claimer, address indexed recipient, uint256 yieldFee);
+
+    /* ============ Custom Errors ============ */
+
+    /**
+     * @notice Emitted when there is insufficient balance to decrement from `account`.
+     * @param  account The account with insufficient balance.
+     * @param  balance The balance of the account.
+     * @param  amount  The amount to decrement.
+     */
+    error InsufficientBalance(address account, uint256 balance, uint256 amount);
+
+    /// @notice Emitted if no yield is available to claim.
+    error NoYield();
+
+    /// @notice Emitted in constructor if Yield Recipient is 0x0.
+    error ZeroYieldRecipient();
+
+    /* ============ Interactive Functions ============ */
+
+    /**
+     * @notice Claims `recipient`'s accrued yield.
+     * @dev    Can be used to claim yield on behalf of `recipient`.
+     * @param  recipient The address of the recipient.
+     */
+    function claimYieldFor(address recipient) external returns (uint256);
+
+    /**
+     * @notice Claims `recipient`'s accrued yield fee.
+     * @dev    Can be used to claim yield fee on behalf of `recipient`.
+     * @dev    MUST revert if the recipient is address zero.
+     * @dev    SHOULD return early if the claimable yield fee is zero.
+     * @param  recipient The address of the recipient.
+     * @return The amount of yield fee claimed.
+     */
+    function claimYieldFeeFor(address recipient) external returns (uint256);
+
+    /* ============ View/Pure Functions ============ */
+
+    /**
+     * @notice Returns the yield accrued for `account`, which is claimable.
+     * @param  account The account being queried.
+     * @return The amount of yield that is claimable.
+     */
+    function accruedYieldOf(address account) external view returns (uint240);
+
+    /**
+     * @notice Returns the token balance of `account` including any accrued yield.
+     * @param  account The address of some account.
+     * @return The token balance of `account` including any accrued yield.
+     */
+    function balanceWithYieldOf(address account) external view returns (uint256);
+
+    /**
+     * @notice Returns the principal of `account`.
+     * @param  account The address of some account.
+     * @return The principal of `account`.
+     */
+    function principalOf(address account) external view returns (uint112);
+
+    /// @notice The projected total supply if all accrued yield was claimed at this moment.
+    function projectedSupply() external view returns (uint240);
+
+    /// @notice The total accrued yield claimable by holders.
+    function totalAccruedYield() external view returns (uint240);
+
+    /// @notice The total principal to help compute `totalAccruedYield()`, and thus `excess()`.
+    function totalPrincipal() external view returns (uint112);
+}
