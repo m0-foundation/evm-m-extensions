cdb9dc4e266661097502dace1e831dc07cefbbf2
Pierrick Turelier <pierrick@turelier.com>
2025-07-28 15:32:43 -0500

    feat(MYieldToOne): add internal init function

--- src/projects/yieldToOne/MYieldToOne.sol
+++ src/projects/yieldToOne/MYieldToOne.sol
@@ -55,12 +55,12 @@ contract MYieldToOne is IMYieldToOne, MYieldToOneStorageLayout, MExtension, Blac
 
     /**
      * @dev   Initializes the M extension token with yield claimable by a single recipient.
-     * @param name                   The name of the token (e.g. "M Yield to One").
-     * @param symbol                 The symbol of the token (e.g. "MYO").
-     * @param yieldRecipient_        The address of an yield destination.
-     * @param admin           The address of a admin.
-     * @param blacklistManager       The address of a blacklist manager.
-     * @param yieldRecipientManager  The address of a yield recipient setter.
+     * @param name                  The name of the token (e.g. "M Yield to One").
+     * @param symbol                The symbol of the token (e.g. "MYO").
+     * @param yieldRecipient_       The address of a yield destination.
+     * @param admin                 The address of an admin.
+     * @param blacklistManager      The address of a blacklist manager.
+     * @param yieldRecipientManager The address of a yield recipient setter.
      */
     function initialize(
         string memory name,
@@ -70,6 +70,26 @@ contract MYieldToOne is IMYieldToOne, MYieldToOneStorageLayout, MExtension, Blac
         address blacklistManager,
         address yieldRecipientManager
     ) public virtual initializer {
+        __MYieldToOne_init(name, symbol, yieldRecipient_, admin, blacklistManager, yieldRecipientManager);
+    }
+
+    /**
+     * @notice Initializes the MYieldToOne token.
+     * @param name                  The name of the token (e.g. "M Yield to One").
+     * @param symbol                The symbol of the token (e.g. "MYO").
+     * @param yieldRecipient_       The address of a yield destination.
+     * @param admin                 The address of an admin.
+     * @param blacklistManager      The address of a blacklist manager.
+     * @param yieldRecipientManager The address of a yield recipient setter.
+     */
+    function __MYieldToOne_init(
+        string memory name,
+        string memory symbol,
+        address yieldRecipient_,
+        address admin,
+        address blacklistManager,
+        address yieldRecipientManager
+    ) internal onlyInitializing {
         if (yieldRecipientManager == address(0)) revert ZeroYieldRecipientManager();
         if (admin == address(0)) revert ZeroAdmin();
 
